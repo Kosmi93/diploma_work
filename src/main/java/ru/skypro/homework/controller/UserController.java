102fc8a8d343e8d9d581c81e68616097d457470e
meunier-github <meunier.natalia@gmail.com>
2025-03-27 17:59:47 +0100

    Добавлены dto controller для регистрации пользователя

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.SetPasswordDto;
+import ru.skypro.homework.dto.UpdateUserDto;
+import ru.skypro.homework.dto.UserDto;
+
+import javax.validation.Valid;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @PostMapping("/set_password")
+    public ResponseEntity<Void> setPassword(@RequestBody SetPasswordDto password) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getAuthUser() {
+        UserDto user = null;
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UpdateUserDto updateUser) {
+        UserDto updatedUser = null;
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<UserDto> updateUserImage(@RequestParam("image") MultipartFile image) {
+        UserDto user = null;
+        return ResponseEntity.ok(user);
+    }
+}
