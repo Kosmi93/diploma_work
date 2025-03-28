1a617600d4be1d5327cea84a1e74ea240c346dc4
meunier-github <meunier.natalia@gmail.com>
2025-03-28 17:45:11 +0100

    добавлена model

--- src/main/java/ru/skypro/homework/dto/LoginDto.java
+++ src/main/java/ru/skypro/homework/dto/LoginDto.java
@@ -1,24 +1,4 @@
-/*
-package ru.skypro.homework.dto;
 
-import lombok.*;
-import lombok.experimental.Accessors;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
-@Data
-public class LoginDto {
-
-    @Size(min = 4, max = 32, message = "Логин должен содержать от 4 до 32 символов")
-    @NotBlank(message = "Логин обязателен для заполнения")
-    private String userName;
-    @Size(min = 8, max = 16, message = "Пароль должен содержать от 8 до 16 символов")
-    @NotBlank(message = "Пароль обязателен для заполнения")
-    private String password;
-
-}
-*/
 package ru.skypro.homework.dto;
 
 import lombok.Data;
